5f52bb0676fa7dcc66b2a9697625c55befa8cfaf
Shahmir Rasul <shahmirrasul@gmail.com>
2026-05-13 00:57:12 +1000

    added js dynamic rendering for product page + initial image header for product page

--- products.js
+++ products.js
@@ -1,3 +1,7 @@
+const top_images = {
+    bats: "assets/pngimg.com - cricket_PNG27.png"
+};
+
 const products = {
     bats: [
         {name: "Gray Niccols"},
@@ -18,4 +22,23 @@ const category = param.get("type");
 
 //set page title
 document.getElementById("product-header").textContent = category ? category.toUpperCase(): "Products";
+document.getElementById("breadcrumb-item").textContent = category ? category.toUpperCase(): "Products";
+const grid = document.getElementById("product-grid");
+const top_area = document.getElementById("top-image");
+const img_comp = document.createElement("img");
 
+if (top_images[category]){
+    img_comp.src=top_images[category];
+    img_comp.classList.add("top_img");
+    top_area.appendChild(img_comp);
+
+}
+if (products[category]){
+    products[category].forEach(item=>{
+
+        const card = document.createElement("div");
+        card.classList.add("product-card");
+        card.innerHTML= `<h3>${item.name}</h3>`
+        grid.appendChild(card);
+    });
+}
